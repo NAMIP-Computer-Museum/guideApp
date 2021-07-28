3fac5237a9df43c067d39f0a1aafcb8cf99c184e
Aurelien Masson <aurelien.masson@outlook.com>
2021-07-28 13:37:20 +0200

    Utilisation de la base de données

--- Components/FriseChronologique/Frise.js
+++ Components/FriseChronologique/Frise.js
@@ -2,11 +2,64 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View ,Pressable,Image,TouchableOpacity} from 'react-native';
 import Timeline from 'react-native-timeline-flatlist'
+import * as FileSystem from 'expo-file-system'
+import {Asset} from 'expo-asset'
+
 import ordinateur from '../../Data/DonneesOrdinateur/DonneesOrdiFr.js'
 import computer from '../../Data//DonneesOrdinateur/DonneesOrdiEn.js'
 import i18n from '../../Language/Translate'
+import * as SQLite from 'expo-sqlite'
+
+let db;
 
 class Frise extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      data:[]
+    }
+    this.fetchOrdinateur = this.fetchOrdinateur.bind(this)
+    this.fetchOrdinateur()
+  }
+
+  fetchOrdinateur = async() =>{
+    let dirInfo;
+    try {
+      dirInfo = await FileSystem.getInfoAsync(`${FileSystem.documentDirectory}SQLite`);
+    } catch(err) { Sentry.captureException(err) };
+    if (!dirInfo.exists) {
+      try {
+        await FileSystem.makeDirectoryAsync(`${FileSystem.documentDirectory}SQLite`, { intermediates: true });
+      } catch(err) { Sentry.captureException(err) }
+    };
+    await FileSystem.downloadAsync(Asset.fromModule(require("../../assets/Ordinateur.db")).uri,
+    `${FileSystem.documentDirectory}SQLite/ordinateur.db`);
+    db = SQLite.openDatabase("ordinateur.db")
+
+    let requete
+    if(this.testLocale() === 'EN'){
+      requete = 'Select * from ordinateurEN'
+    }
+    else{
+      requete = 'Select * from ordinateurFR'
+    }
+    db.transaction((tx) => {
+        tx.executeSql(requete,null,
+          (tx,results)=>{
+            var taille = results.rows.length
+            let tableau = []
+            for(let i=0;i<taille;i++){
+              tableau.push(results.rows.item(i))
+            }
+            //console.log(tableau)
+            this.setState({data : tableau})
+          },
+          (tx,error)=>{
+            console.log('error',error)
+          }
+        )
+      })
+    }
 
     renderDetail(rowData,sectionID, rowID){
       let title = <Text style = {styles.title}>{rowData.title}</Text>
@@ -29,9 +82,9 @@ class Frise extends React.Component{
   testLocale(){
     if(i18n.locale === 'en'){
       //console.log(computer)
-      return (computer)
+      return ('EN')
     }
-    return (ordinateur)
+    return ('FR')
     //console.log(ordinateur)
   }
 
@@ -41,12 +94,13 @@ class Frise extends React.Component{
     }
     render(){
       return(
+        //console.log(this.state.data),
         <View style={styles.main}>
           <Text style={styles.text}>{i18n.t('friseTexte')}</Text>
           <Timeline style={styles.timeline}
             timeStyle = {styles.time}
             separator={true}
-            data={this.testLocale()}
+            data={this.state.data}
             onEventPress = {this.onEventPress}
             renderDetail={this.renderDetail}
             />
