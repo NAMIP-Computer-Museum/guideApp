c08600a8ba61ba5d181b24fc78ecd95a14d99d56
Christophe Ponsard <christophe.ponsard@gmail.com>
2021-09-28 12:35:19 +0200

    corrections textes simples

--- Language/Translate.js
+++ Language/Translate.js
@@ -11,17 +11,17 @@ i18n.translations = {
     accueilBoutonQuiz : "Quiz",
     TitreIntroduction : "Introduction",
     DescriptionIntroduction : "Cette application a été conçue pour vous guider dans l'exposition temporaire 'micro-ordinateur, még@ révolution'. Elle s'inscrit dans le cycle 'De l’informatique antique à l’informatique quantique'.\n\n"+
-    "L'exposition retrace l’histoire, depuis les années 1970 jusqu'au milieu des années 1990,"+
-    "de l’émergence d’une informatique interactive, personnelle, conviviale et centrée sur l’utilisateur final."+
-    "Celui-ci peut pour la première acquérir son ordinateur individuel,  par contraste avec l'exposition précédente"+
+    "L'exposition retrace l’histoire, depuis les années 1970 jusqu'au milieu des années 1990, "+
+    "de l’émergence d’une informatique interactive, personnelle, conviviale et centrée sur l’utilisateur final. "+
+    "Celui-ci peut pour la première acquérir son ordinateur individuel,  par contraste avec l'exposition précédente "+
     "consacrée aux ordinateurs des années 1960 très onéreux et réservés aux organisations qui en ont les moyens.\n\n"+
-    "L'exposition ne se concentre pas uniquement sur l'évolution technique des micro-ordinateurs mais illustre aussi"+
+    "L'exposition ne se concentre pas uniquement sur l'évolution technique des micro-ordinateurs mais illustre aussi "+
     "en parallèle, l'émergence des interfaces graphiques utilisateurs et leur intégration dans ces machines grand public.",
     boutonModeEmploi : 'Mode Emploi',
     TitreModeEmploi : "Mode Emploi",
-    DescriptionModeEmploi : "L'application est structurée selon une ligne du temps qui suit le parcours de l'exposition.\n"+
+    DescriptionModeEmploi : "L'application est structurée selon une ligne du temps qui suit le parcours de l'exposition.\n\n"+
     "Afin de se situer aisément dans une phase ou thématique spécifique, l'exposition permet de filtrer aisément "+
-    "selon une des phase temporelle ou l'ensemble. Pour la période concernée, on peut aussi filtrer sur:\n"+
+    "selon une des phase temporelle ou l'ensemble. Pour la période concernée, on peut aussi filtrer sur :\n"+
     "- les micro-ordinateurs présentés dans l'exposition\n"+
     "- les micros-processeurs mis en oeuvre\n"+
     "- les développements des interfaces graphiques\n"+
@@ -56,9 +56,9 @@ i18n.translations = {
     reponseWritingQuestion : "Reponse : ",
     validerReponse : "Valider Reponse",
     TitreApropos : "A Propos",
-    DescriptionApropos : "Cette application a été développée par les bénévoles du musée NAM-IP à des fins de support pour la visite."+
-    "Un guide papier plus complet est en vente à la boutique du musée."+
-    "Vos commentaires d'amélioration sont les bienvenus en donnant un retour à la réception."+
+    DescriptionApropos : "\nCette application a été développée par les bénévoles du musée NAM-IP à des fins de support pour la visite."+
+    "Un guide papier plus complet est en vente à la boutique du musée.\n\n"+
+    "Vos commentaires d'amélioration sont les bienvenus en donnant un retour à la réception.\n\n"+
     "(C) 2021 NAM-IP"
   },
   "en" : {
