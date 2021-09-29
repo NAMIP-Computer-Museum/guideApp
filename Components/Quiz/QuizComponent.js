d22371d04c697148aa24167efe0ba237f227ef7f
Christophe Ponsard <christophe.ponsard@gmail.com>
2021-09-29 12:54:42 +0200

    plus de questions tirage plus grand

--- Components/Quiz/QuizComponent.js
+++ Components/Quiz/QuizComponent.js
@@ -18,7 +18,7 @@ class QuizComponent extends React.Component{
       questionSuivante : false,
       derniereQuestion : false
     }
-    questions = this.randomQuestions(quiz,3);
+    questions = this.randomQuestions(quiz,5);
   }
 
   randomQuestions = (quizData,numberQuestions) =>{
--- assets/Quiz/quizData.js
+++ assets/Quiz/quizData.js
@@ -16,9 +16,9 @@ export default quiz = [
     {
       id : 3,
       questionType : 'multiple',
-      question : 'Quel choix est une Application ?',
+      question : 'Quel choix est une application ?',
       answer : 'Deluxe Paint',
-      allChoices : ['CP/M','Deluxe Paint','MS-DOS','macOS']
+      allChoices : ['CP/M','Deluxe Paint','MS-DOS','MacOS']
     },
     {
       id : 4,
@@ -33,5 +33,40 @@ export default quiz = [
       question : "Qui est l'inventeur de la première souris ?",
       answer : 'Douglas Engelbart',
       allChoices : ['Steve Jobs','Douglas Engelbart','Bill Gates']
-    }
+    },
+    {
+      id : 6,
+      questionType : 'multiple',
+      question : "Quel ordinateur ne fait pas partie de la trinité de 1977 ?",
+      answer : 'IBM PC',
+      allChoices : ['Apple II','Commodore PET','IBM PC','TRS-80']
+    },
+    {
+      id : 7,
+      questionType : 'multiple',
+      question : "Sous quel nom est connu la démonstration des principes fondateurs de l'informatique moderne par D. Engelbart en 1968 ?",
+      answer : 'Mother of all Demos',
+      allChoices : ['Apple Keynote','Mother of all Demos','COMputer Dealers EXhibition']
+    },
+    {
+      id : 8,
+      questionType : 'multiple',
+      question : "Quel est le premier tableur, sorti en 1979 ?",
+      answer : 'VisiCalc',
+      allChoices : ['Lotus 1,2,3', 'Multiplan','SuperCalc','VisiCalc']
+    },
+    {
+      id : 9,
+      questionType : 'multiple',
+      question : "Que signifie WYSIWYG",
+      answer : 'What You See Is What You Get',
+      allChoices : ['When You Sell Is When You Gain', 'What You See Is What You Get','What You Show is What You Give']
+    },
+    {
+      id : 10,
+      questionType : 'multiple',
+      question : "Quelle ordinateur Apple déjà doté d'une interface graphique et d'une souris a précédé le Macintosh ?",
+      answer : 'LISA',
+      allChoices : ['Apple II', 'STAR', 'LISA', 'ZX81']
+    },
 ]
